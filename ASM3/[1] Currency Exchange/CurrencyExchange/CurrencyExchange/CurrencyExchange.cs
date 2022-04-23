b691b9675c0a6335e868c4f27928414482589331
Mostafa <sameh@010036.com>
2022-04-24 00:38:37 +0200

    Algo Asignment

---
+++ ASM3/[1] Currency Exchange/CurrencyExchange/CurrencyExchange/CurrencyExchange.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange
+{
+    // *****************************************
+    // DON"T CHANGE CLASS NAME OR FUNCTION NAME
+    // *****************************************
+    public static class CurrencyExchange
+    {
+        //Your Code is Here:
+        //==================
+        /// <summary>
+        /// find the index of the USD Dollar $ with the smallest number of questions.
+        /// </summary>
+        /// <param name="N">Number of customers (N)</param>
+        /// <param name="M">Number of currencies (M)</param>
+        /// <param name="knows">N*M Matrix indicating whether customer i knows currency j or not</param>
+        /// <returns>index of US Dollar</returns>
+        public static int CheckUSD(int N, int M, bool[,] knows)
+        {
+            //knows[P,C]=true if person P knows currency C and knows[P,C]=false if person P doesn't know Currency C.
+            int c = 0, r = 0;
+            while(c<N)
+            {
+                if (knows[c, r])
+                { c++; }
+
+                else
+                {r++;}
+            }
+
+            return r;
+        }
+    }
+}
---
+++ ASM4/Is it a Tree/Is it a Tree/IsTree/CheckTree.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IsATree
+{
+    public class CheckTree
+    {
+        //===============================================================================
+        //Your Code is Here:
+        public static bool test = true;
+        public static bool IsTree(string[] vertices, List<KeyValuePair<string, string>> edges)
+        {
+
+            Dictionary<String, List<String>> arr = new Dictionary<String, List<String>>();
+            Dictionary<string, string> color= new Dictionary<string, string>();
+            HashSet<string> ver = new HashSet<string>();
+            test = true;
+            foreach(var Pair in edges )
+            {
+                if (arr.ContainsKey(Pair.Key))
+                { 
+                    arr[Pair.Key].Add(Pair.Value);
+                    
+                        
+                }
+                else
+                {
+                    color[Pair.Key] = "w";
+                    ver.Add(Pair.Key);
+                    arr.Add(Pair.Key, new List<string>());
+                    arr[Pair.Key].Add(Pair.Value);
+                }
+                if (!arr.ContainsKey(Pair.Value))
+                {
+                    color[Pair.Value] = "w";
+                    ver.Add(Pair.Value);
+                    arr.Add(Pair.Value, new List<string>());
+                }
+
+            }
+            
+           
+
+            foreach(var v in ver)
+            {
+                if(!test)
+                {
+                    return false;
+                }
+                if(color[v]=="w")
+                {
+                    //Console.WriteLine(arr[v].ElementAt(0));
+                    dfs_visit(color,v ,arr);
+                    
+                }
+                
+            }
+            return true;
+        }
+        public static void dfs_visit (Dictionary<string, string> color, string node, Dictionary<String, List<String>> adj)
+        {
+           
+                color[node] = "g";
+            
+               
+                foreach (var p in adj[node])
+                {
+                    if(color[p] == "w")
+                    {
+                      dfs_visit(color, p, adj);
+                    }
+                    else if (color[p] == "g")
+                    {
+                        test = false;
+                        return;
+                    }
+                   
+                }
+                color[node] = "b";
+              
+          
+        }
+        
+
+        //===============================================================================
+       
+        //===============================================================================
+    }
+}
